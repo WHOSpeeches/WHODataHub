2ff1e6995289f249c8ae577967f04bd57d247c09
Mark Newman <mark@trinetteandmark.com>
2023-01-26 22:55:44 -0500

    rename

--- src/WHOSpeeches/utils/const.py
+++ src/WHOSpeeches/utils/const.py
@@ -1,5 +1,6 @@
 USER_AGENT = 'WHOSpeechAnalysis/1.0'
 URL_TLD = 'http://who.int'
+API_SPEECHES = f'{URL_TLD}/api/hubs/speeches?$orderby=PublicationDateAndTime&$select=Title,ItemDefaultUrl,FormatedDate&$skip={{skip}}'
 URL_ROBOTS = f'{URL_TLD}/robots.txt'
 URL_SPEECHES = f'{URL_TLD}/director-general/speeches/{{page}}'
 WEB_DELAY = 2
